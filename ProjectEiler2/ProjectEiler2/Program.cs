7028a686697ace21fda3320ffaf307fe063646c5
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-05-20 20:34:44 +0300

    ProjectEuler2

---
+++ ProjectEiler2/ProjectEiler2/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> numFibonachi = new List<int>();
+int sumFib = 2;
+
+numFibonachi.Add(1);
+numFibonachi.Add(2);
+
+for (int i = 2; numFibonachi[i-1] < 4_000_001; i++)
+{
+    numFibonachi.Add(numFibonachi[i-2] + numFibonachi[i-1]);
+
+    if (numFibonachi[i] % 2 == 0)
+    {
+        sumFib += numFibonachi[i];
+    }
+}
+
+Console.WriteLine(sumFib);
