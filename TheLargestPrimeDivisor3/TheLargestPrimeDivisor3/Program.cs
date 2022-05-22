8dcf2aabe9d512690f20fa5938098055acb0f388
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-05-22 18:08:55 +0300

    RTS

---
+++ TheLargestPrimeDivisor3/TheLargestPrimeDivisor3/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 13195;
+bool[] numbersErastofen = new bool[n];
+numbersErastofen[0] = numbersErastofen[1] = false;
+for (int i = 2; i < n; i++)
+{
+    if (numbersErastofen[i])
+    {
+        if (i * i <= n)
+        {
+            for (int j = i * i; j <= n; j += i)
+                numbersErastofen[j] = false;
+        }
+    }
+}
