841a1620338fe0c8b922bff68801cbf6b0c57937
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-05-24 06:28:59 +0300

    AddTask3

--- TheLargestPrimeDivisor3/TheLargestPrimeDivisor3/Program.cs
+++ TheLargestPrimeDivisor3/TheLargestPrimeDivisor3/Program.cs
@@ -1,14 +1,37 @@
 ﻿int n = 13195;
-bool[] numbersErastofen = new bool[n];
-numbersErastofen[0] = numbersErastofen[1] = false;
+long nn = 600851475143;
+bool[] sinpleNumbers = new bool[n];
+List<long> numOK = new();
+sinpleNumbers[0] = sinpleNumbers[1] = true;
+
 for (int i = 2; i < n; i++)
 {
-    if (numbersErastofen[i])
+    if (!sinpleNumbers[i])
     {
-        if (i * i <= n)
+        for (int j = i; j < n; j++)
         {
-            for (int j = i * i; j <= n; j += i)
-                numbersErastofen[j] = false;
+            if (j % i == 0 && i != j)
+            {
+                sinpleNumbers[j] = true;
+            }
         }
     }
+}
+
+for (int i = 0; i < n; i++)
+{
+    while (!sinpleNumbers[i] && nn % i == 0 && nn != 0)
+    {
+            nn /= i;
+            numOK.Add(i);
+    }
+    if(nn == 1 || nn == 0)
+    {
+        break;
+    }
+}
+
+foreach (var item in numOK)
+{
+    Console.WriteLine(item);
 }
