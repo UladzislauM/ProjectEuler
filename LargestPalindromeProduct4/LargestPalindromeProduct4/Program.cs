c5aadfb51f3d6ff7e6a5b9aa924134a334a13cdd
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-05-26 09:00:34 +0300

    PolindromTask4

---
+++ LargestPalindromeProduct4/LargestPalindromeProduct4/Program.cs
@@ -0,0 +1,34 @@
+﻿int iterations = 0;
+
+for (int a = 999; a >= 0; a--)
+{
+    string SNumberLeft = a.ToString();
+    string SNumberRight = Reverse(a.ToString());
+    int aR = Convert.ToInt32(SNumberRight);
+
+    int R = a * 1000 + aR;
+    int b = 999;
+    int s = R / b;
+
+    while (b >= s)
+    {
+        iterations++;
+        if (R == b * s)
+        {
+            Console.WriteLine($"multiplier 1 = {s}; multiplier = 2 = {b}; polindrom = {R} iteration = {iterations}");
+            return;
+        }
+        else
+        {
+            b--;
+            s = R / b;
+        }
+    }
+}
+
+string Reverse(String s)
+{
+    char[] arr = s.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+}
