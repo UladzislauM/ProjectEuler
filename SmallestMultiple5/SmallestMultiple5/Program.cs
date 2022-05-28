ed5fd5d4d61b8f28b79f8e419e58549df275a97b
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-05-28 05:45:47 +0300

    Start 5 task

---
+++ SmallestMultiple5/SmallestMultiple5/Program.cs
@@ -0,0 +1,10 @@
+﻿int smallestMultiple = 0;
+
+for (int number = 400; ; number++)
+{
+    int curDig = number / Convert.ToInt32(Math.Pow(10, 2));
+    if ((number % 100) % 10 == 0 && curDig % 2 == 0)
+    {
+
+    }
+}
